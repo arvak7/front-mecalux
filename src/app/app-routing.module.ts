399867a7c5866b22dfb8aca95cdc5af51377ce39
Manel Reus Giner <manel.Giner@serviceware-se.com>
2023-01-20 13:00:23 +0100

    Create Warehouse, Rack, Permutation and Navegatio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PermutationsComponent } from './permutations/permutations.component';
+import { RackComponent } from './rack/rack.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'warehouse', component: WarehouseComponent },
+  { path: 'racks', component: RackComponent },
+  { path: 'permutations', component: PermutationsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
+import { RackComponent } from './rack/rack.component';
+import { PermutationsComponent } from './permutations/permutations.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarehouseComponent,
+    RackComponent,
+    PermutationsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
